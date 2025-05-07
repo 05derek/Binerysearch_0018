6f9b989bfe522bbcfd71e21959f072f592d7b23f
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-07 20:05:51 +0700

    bubblesortArray

--- Liner&BinerySearch.cpp
+++ Liner&BinerySearch.cpp
@@ -1,13 +1,18 @@
 
-
-
-cout << "==============================" << endl;
-    cout << "Masukkan Elemen Array" << endl;
-    cout << "==============================" << endl;
-
-    for (int i = 0; i < nPanjang; i++)
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
     {
-        cout << "Data ke-" << (i + 1) << ": ";
-        cin >> elemen[i];
-    }
+        for (int i = 0; i <= nPanjang - pass - 1; i++)
+        {
+            if (elemen[i] > elemen[i + 1])
+            {
+                int temp = elemen[i];
+                elemen[i] = elemen[i + 1];
+                elemen[i + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass < nPanjang);
 }
