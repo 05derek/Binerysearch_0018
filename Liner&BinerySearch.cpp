f121b52d3dd9901a78f3904de74d30178e1c62ce
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-07 20:13:26 +0700

    Last commit

--- Liner&BinerySearch.cpp
+++ Liner&BinerySearch.cpp
@@ -1,14 +1,8 @@
 
-            else
-            {
-                low = mid + 1;
-            }
-        } while (low <= high);
-
-        cout << "\nMaaf! Elemen " << item << " tidak ditemukan dalam array.\n";
-
-        cout << "\nIngin mencari lagi? (y/n): ";
-        cin >> ulang;
-
-    } while (ulang == 'y' || ulang == 'Y');
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    binarySearch();
 }
