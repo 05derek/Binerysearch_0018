df753bbae2513345e97ca55abf264d7260320ec4
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-07 20:10:42 +0700

    Lanjutan biner search part 2

--- Liner&BinerySearch.cpp
+++ Liner&BinerySearch.cpp
@@ -1,4 +1,10 @@
 
-        do
-        {
-            int mid = (low + high) / 2;
+            if (elemen[mid] == item)
+            {
+                cout << "\nElemen " << item << " ditemukan pada indeks ke-" << mid << ".\n";
+                return;
+            }
+            else if (item < elemen[mid])
+            {
+                high = mid - 1;
+            }
