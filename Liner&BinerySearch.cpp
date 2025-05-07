d58b2afd7def6a68b52f005d8f7537f9994b6e7e
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-07 20:04:56 +0700

    cout input

--- Liner&BinerySearch.cpp
+++ Liner&BinerySearch.cpp
@@ -1,19 +1,13 @@
 
 
-void input()
-{
-    while (true)
-    {
-        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
-        cin >> nPanjang;
 
-        if (nPanjang <= 10)
-        {
-            break;
-        }
-        else
-        {
-            cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
-        }
+cout << "==============================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "==============================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> elemen[i];
     }
 }
